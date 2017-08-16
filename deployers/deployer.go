7cf4ad0e8b9bc000a0144c1349e8f7bcf4bcadf7
Olivier Biesmans <olivier.biesmans@blablacar.com>
2017-08-16 17:26:06 +0200

    First take on deploy, misc fix

--- deployers/deployer.go
+++ deployers/deployer.go
@@ -3,4 +3,5 @@ package deployers
 type Deployer interface {
 	ListVersions(env string) (map[string]string, error)
 	ListVcsVersions(env string) ([]string, error)
+	Deploy(env string, podVersion string) error
 }
