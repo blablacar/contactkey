0160ca3feb5a1e22521559af8e1278caae640b1c
Olivier Biesmans <olivier.biesmans@blablacar.com>
2017-10-11 10:11:20 +0200

    Enable log to journal only when possible

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 
 	"runtime/debug"
 
+	"github.com/coreos/go-systemd/journal"
 	"github.com/remyLemeunier/contactkey/commands"
 	log "github.com/sirupsen/logrus"
 	"github.com/wercker/journalhook"
 )
 
 func main() {
-	log.AddHook(&journalhook.JournalHook{})
+	if journal.Enabled() {
+		log.AddHook(&journalhook.JournalHook{})
+	}
+
 	defer func() {
 		if r := recover(); r != nil {
 			log.WithFields(log.Fields{"stacktrace": string(debug.Stack())}).Error(fmt.Sprintf("Recovered from panic %q", r))
